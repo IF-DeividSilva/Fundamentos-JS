f0d8c69f1c86223f1d8ad8ac6f2df5165703b827
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2024-01-08 14:37:35 -0300

    commit

---
+++ Listas JS/Functions/Ex1.js
@@ -0,0 +1,15 @@
+// Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração, 
+// multiplicação e divisão desses valores
+
+function soma (a,b) {
+        soma = a + b;
+        sub = a - b;
+        mult = a * b;
+        div = a / b;
+    
+        console.log( soma+'\n',sub+'\n' , mult+'\n' , div+'\n');
+}
+    const a = 5
+    const b = 10
+    
+    soma(a, b);
---
+++ Listas JS/Functions/Ex3.js
@@ -0,0 +1,8 @@
+//  Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function exponencial(base, expoente){
+    const resultado = base ** expoente
+    return resultado
+}
+
+console.log(exponencial(2,5));
