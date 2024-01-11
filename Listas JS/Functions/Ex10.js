a6449b997cad7987c010cac6023a1d774a532bf3
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2024-01-11 11:12:44 -0300

    commit

---
+++ Listas JS/Functions/Ex10.js
@@ -0,0 +1,16 @@
+/*
+10) ​Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true 
+ou false
+*/
+
+function verificar(n){
+    if (n%3 == 0)
+        return true;
+    else
+        return false;    
+}
+
+
+console.log (verificar(30));
+console.log (verificar(10));
+console.log (verificar(150));
